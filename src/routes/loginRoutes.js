06cef1b380972b9d0df9760244bed1dd1a9cc809
maheshsaveasweb <mahesh.saveasweb101@gmail.com>
2024-10-17 17:15:33 +0530

    created dashboard login

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -33,7 +33,7 @@ router.post('/', async (req, res) => {
 
         // Generate JWT token
         const token = jwt.sign({ userId: user.id }, 'your_jwt_secret', {
-            expiresIn: '1h',
+
         });
 
         res.json({
@@ -49,5 +49,47 @@ router.post('/', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
+router.post('/dashboard', async (req, res) => {
+    const { email, password } = req.body;
 
+    if (!email || !password) {
+        return res.status(400).json({ error: 'Email and password are required' });
+    }
+
+    try {
+        // Find user by username
+        const user = await prisma.user.findUnique({
+            where: { email },
+        });
+
+        if (!user) {
+            return res.status(401).json({ error: 'Invalid credentials' });
+        }
+
+        // Compare passwords
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({ error: 'Invalid credentials' });
+        }
+        if (user.userType != "Superadmin") {
+            return res.status(401).json({ error: 'Invalid credentials' });
+        }
+        // Generate JWT token
+        const token = jwt.sign({ userId: user.id }, 'your_jwt_secret', {
+
+        });
+
+        res.json({
+            success: true, token, user: {
+                name: user.name,
+                email: user.email,
+                userType: user.userType
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 module.exports = router;
