607ad39f3e21783af2d0f970245ad2fa7b651642
SaveaswebM <164156367+SaveaswebM@users.noreply.github.com>
2024-10-24 16:19:50 +0530

    Update loginRoutes.js

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -92,4 +92,39 @@ router.post('/dashboard', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
+
+router.delete("/", async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json({ error: 'Email and password are required' });
+    }
+    try {
+        const user = await prisma.user.findUnique({
+            where: { email },
+        });
+        if (!user) {
+            return res.status(401).json({ error: 'Invalid credentials' });
+        }
+
+        // Compare passwords
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({ error: 'Invalid credentials' });
+        }
+        // Delete the user
+        await prisma.user.delete({
+            where: { email },
+        });
+
+        return res.status(200).json({ success: "Account deleted successfully" });
+
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+
+
+});
 module.exports = router;
